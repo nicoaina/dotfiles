e79716d80ff4da2ccaba21a6651da0256d54ec1b
nicoaina <nicolas.martin@deveryware.net>
2017-04-02 20:09:28 +0200

    First commit

---
+++ vim/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# Install vim dependencies : vundle
+[ -d ~/.vim ] || mkdir ~/.vim
+[ -d ~/.vim/bundle ] || mkdir ~/.vim/bundle
+[ -d ~/.vim/bundle/vundle ] || git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+#vim +PluginInstall +qall now
