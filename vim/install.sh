1213650766801e61d239e178e2d58d1209a351d9
nicoaina <nico.aina@gmail.com>
2018-03-23 19:11:14 +0100

    setup vim and vundle

--- vim/install.sh
+++ vim/install.sh
@@ -4,3 +4,10 @@ PACKAGE_NAME="vim"
 
 # Check if autojump is installed. If not, install it.
 dpkg -l | grep -qw $PACKAGE_NAME || sudo apt-get update && sudo apt-get -y install $PACKAGE_NAME
+
+# Install Vundle to manage vim plugin
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install plugin
+vim +PluginInstall +qall
+
